0d4ead5ad2956c57af0b0acfe7e056f1a515f1de
twogog <you@example.com>
2024-04-30 19:25:37 +0600

    feat: SumAll, SumAllTails into slices

--- slices/sum.go
+++ slices/sum.go
@@ -1,5 +1,7 @@
 package main
 
+import "slices"
+
 func Sum(numbers []int) int {
 	sum := 0
 
@@ -8,4 +10,31 @@ func Sum(numbers []int) int {
 	}
 
 	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	var result []int
+
+	for _, numbers := range numbersToSum {
+		result = append(result, Sum(numbers))
+	}
+
+	return result
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var result []int
+
+	for _, numbers := range numbersToSum {
+		var fakeZero []int
+		if len(numbers) == 0 {
+			fakeZero = slices.Concat([]int{0}, numbers)
+		} else {
+			fakeZero = numbers
+		}
+
+		result = append(result, Sum(fakeZero[1:]))
+	}
+
+	return result
 }
